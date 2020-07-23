374a43069ceff7632b1294ec01cdc7dda90c5a65
Daryl <darylgialolo@gmail.com>
2020-07-23 13:06:41 +0800

    Option 4 done

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -290,7 +290,7 @@ public class ResourceCentre {
 
 	public static boolean doReturnChromebook(ArrayList<Chromebook> chromebookList, String tag){
 		boolean isReturned = false;
-		// write your code heree
+		// write your code here
 		for (int i = 0; i < chromebookList.size(); i++) {
 			if (tag.equalsIgnoreCase(chromebookList.get(i).getAssetTag())
 					&& chromebookList.get(i).getIsAvailable() == false) {
